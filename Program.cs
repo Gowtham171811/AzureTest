2f30cd7b32e398709b7b11a6ead627d426841530
Gowtham171811 <gowtham.sk@mitrahsoft.com>
2026-05-08 17:33:54 +0530

    program change

--- Program.cs
+++ Program.cs
@@ -53,12 +53,13 @@ builder.Services.AddScoped<AuthService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
+
 
 app.UseHttpsRedirection();
 
